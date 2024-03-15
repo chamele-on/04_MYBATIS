683ccd64cc13ec34ab08caddba54b1d92651fcec
chamele-on <skykitty13@naver.com>
2024-03-15 16:20:07 +0900

    2024-03-15

--- 01_lecture-source/chap05-mapper-element-lecture-source/src/main/java/com/ohgiraffers/section01/xmlmapper/ElementTestService.java
+++ 01_lecture-source/chap05-mapper-element-lecture-source/src/main/java/com/ohgiraffers/section01/xmlmapper/ElementTestService.java
@@ -19,16 +19,18 @@ public class ElementTestService {
         mapper = sqlSession.getMapper(ElementTestMapper.class);
 
         for (int i = 0; i < 10; i++) {
+            // 조회 시간 확인 용 시작 시간
             Long startTime = System.currentTimeMillis();
 
             List<String> nameList = mapper.selectCacheTest();
             System.out.println(nameList);
-
+            // 조회 시간 확인 용 끝나는 시간
             Long endTime = System.currentTimeMillis();
 
             Long interval = endTime - startTime;
             System.out.println("수행 시간 : " + interval + "(ms)");
         }
+
         sqlSession.close();
 
     }
